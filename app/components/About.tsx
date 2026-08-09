acd411283d2c0ad0c982b27155a4f9966ee54d86
Vynce Labrador <vynce,labrador@ffuf.de>
2026-08-09 18:03:03 +0800

    update: placeholder images

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -51,7 +51,7 @@ export const About = () => {
         <Image
           className="rounded-lg absolute object-cover"
           loading="eager"
-          src="https://images.unsplash.com/photo-1501504905252-473c47e087f8?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 1074w"
+          src="/assets/images/watermark.png"
           alt="About Image"
           fill
         />
@@ -90,7 +90,7 @@ export const About = () => {
         <Image
           className="rounded-lg absolute object-cover"
           loading="eager"
-          src="https://images.unsplash.com/photo-1501504905252-473c47e087f8?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 1074w"
+          src="/assets/images/watermark.png"
           alt="About Image"
           fill
         />
@@ -105,7 +105,7 @@ export const About = () => {
         <Image
           className="rounded-lg absolute object-cover"
           loading="eager"
-          src="https://images.unsplash.com/photo-1501504905252-473c47e087f8?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 1074w"
+          src="/assets/images/watermark.png"
           alt="About Image"
           fill
         />
