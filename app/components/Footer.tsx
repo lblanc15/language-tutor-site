3afbcac6859ec6a03ee61bb5496802205b285ed7
Vynce Labrador <vynce,labrador@ffuf.de>
2026-08-27 21:36:31 +0800

    feat: add canonical links and footer adjustments

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import { SocialIcon } from "react-social-icons";
+import { LucideCopyright } from "lucide-react";
+import Image from "next/image";
 
 export const Footer = () => {
   return (
-    <footer className="w-full px-6 lg:px-12 grid grid-cols-2 gap-4 md:grid-cols-[1fr_1.5fr_1.5fr] lg:grid-cols-3 bg-blue-950 py-12 lg:py-12">
-      <div className="flex justify-center md:justify-start col-span-2 md:col-span-1 mb-8 md:mb-0">
-        <img className="h-24 w-24 lg:h-32 lg:w-32 bg-slate-950" alt="footer-img" src="/assets/images/watermark.png" />
+    <footer className="w-full px-6 lg:px-12 grid grid-cols-1 gap-4 md:grid-cols-[1fr_1.5fr_1.5fr] lg:grid-cols-3 bg-blue-950 py-12 lg:py-12">
+      <div className="flex justify-center md:justify-start md:col-span-1 mb-8 md:mb-0">
+        <div className="h-32 lg:h-full w-32 relative border mb-4">
+          <Image className="absolute object-cover" alt="footer-img" src="/assets/images/watermark.png" fill/>
+        </div>
       </div>
-      <div className="flex flex-col items-center justify-center h-full font-body">
+      <div className="flex flex-col lg:items-center justify-center h-full font-body">
         <ul className="p-0 mt-4 text-white font-light flex flex-col gap-2">
           <li className="text-xs">
             <a
@@ -52,31 +55,12 @@ export const Footer = () => {
           </li>
         </ul>
       </div>
-      <div className="text-white text-sm flex flex-col h-full justify-center items-center">
-        <div>
-          <div className="font-semibold">Academia de Español Rico</div>
-          <p className="text-xs mb-2 font-light font-body">Connect with us on social</p>
-          <div className="flex gap-1 justify-center md:justify-start">
-            <SocialIcon
-              url="https://www.facebook.com/ricospanishacademy"
-              target="_blank"
-              style={{ height: 25, width: 25 }}
-            />
-            <SocialIcon
-              url="https://www.tiktok.com/@academiadeespanolrico"
-              target="_blank"
-              style={{ height: 25, width: 25 }}
-            />
-            <SocialIcon
-              url="mailto:ricospanishacademy@gmail.com"
-              target="_blank"
-              style={{ height: 25, width: 25 }}
-            />
-            <SocialIcon
-              url="https://wa.me/639206126888"
-              target="_blank"
-              style={{ height: 25, width: 25 }}
-            />
+      <div className="text-white text-sm flex flex-col w-full h-full justify-center items-center">
+        <div className="text-[11px] font-body text-white/90">
+          <h2 className="text-sm font-bold mb-2">Academia de Español Rico</h2>
+          <p className="lg:max-w-3/4 mb-4">Online Spanish classes for Filipino learners who want to build confidence and open doors to more opportunities.</p>
+          <div className="flex gap-1 items-center">
+            <LucideCopyright size={11}/>2026 Academia de Español Rico. All rights reserved.
           </div>
         </div>
       </div>
